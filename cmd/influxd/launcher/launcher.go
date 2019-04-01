796803b8a638b50cd9e823c26fa6d50ab6e748a1
Michael Desa <mjdesa@gmail.com>
2019-04-01 11:01:38 -0400

    feat(cmd/influxd/launcher): set org limit service on api backend

--- cmd/influxd/launcher/launcher.go
+++ cmd/influxd/launcher/launcher.go
@@ -639,6 +639,7 @@ func (m *Launcher) run(ctx context.Context) (err error) {
 		ProtoService:                    protoSvc,
 		DocumentService:                 m.kvService,
 		OrgLookupService:                m.kvService,
+		OrgLimitService:                 m.kvService,
 	}
 
 	// HTTP server
